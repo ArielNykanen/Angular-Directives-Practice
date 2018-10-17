e144719b225242f0af6aa380447e48329bae29d9
Ariel Nykanen <arielnykan3n@gmail.com>
2018-10-17 17:25:23 +0300

    Directives Practice

---
+++ src/app/better-high-light.directive.spec.ts
@@ -0,0 +1,8 @@
+import { BetterHighLightDirective } from './better-highLight/better-high-light.directive';
+
+describe('BetterHighLightDirective', () => {
+  it('should create an instance', () => {
+    const directive = new BetterHighLightDirective();
+    expect(directive).toBeTruthy();
+  });
+});
